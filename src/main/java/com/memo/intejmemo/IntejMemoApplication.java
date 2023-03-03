1abf0fc68c4c577d37c619b86a04e9b6ed9d6a83
이경용 <sleekydz86@naver.com>
2023-03-03 11:48:19 +0900

    jpa 메모장 만들기 리마인드

---
+++ src/main/java/com/memo/intejmemo/IntejMemoApplication.java
@@ -0,0 +1,13 @@
+package com.memo.intejmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntejMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntejMemoApplication.class, args);
+	}
+
+}
