3a7137c7c46653fc90bbcfa21ae9ea1d57b83c20
이경용 <sleekydz86@icloud.com>
2023-11-29 01:04:36 +0900

    환경셋팅
    환경셋팅

--- src/main/java/com/memo/intejmemo/controller/MainController.java
+++ src/main/java/com/memo/intejmemo/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.memo.intejmemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +9,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
     @RequestMapping(value = {"/", "index","memoList"} , method = {RequestMethod.GET, RequestMethod.POST})
-    public void IndexMemo(Model model){
+    public void IndexMemo(HttpServletRequest request, Model model){
+        String httpMethod = request.getMethod();
 
+        if ("GET".equals(httpMethod)) {
+            // GET 요청 처리
+        } else if ("POST".equals(httpMethod)) {
+            // POST 요청 처리
+        }
+
+        // 공통 처리 로직
     }
 }
