dfee698da4ef1fcf4573fdfe90776453bbb4a0ae
이경용 <sleekydz86@icloud.com>
2023-12-03 17:51:47 +0900

    AttrRepositoryTDD 업로드

--- src/test/java/com/memo/base/attr/repository/AttrRepositoryTest.java
+++ src/test/java/com/memo/base/attr/repository/AttrRepositoryTest.java
@@ -1,4 +1,51 @@
 package com.memo.base.attr.repository;
 
+
+import com.memo.intejmemo.base.attr.entity.Attr;
+import com.memo.intejmemo.base.attr.repository.AttrRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
 public class AttrRepositoryTest {
+
+    @Autowired
+    private AttrRepository attrRepository;
+
+    @DisplayName("attr 저장")
+    @Test
+    void attr저장(){
+        Attr attr = Attr.builder()
+                .createDate(LocalDateTime.now())
+                .name("age")
+                .build();
+
+        attrRepository.save(attr);
+        assertThat(attr.getId()).isGreaterThan(0L);
+    }
+
+    @DisplayName("attr 저장, 한번 더")
+    @Test
+    void attr저장_한번더(){
+        Attr attr = Attr.builder()
+                .createDate(LocalDateTime.now())
+                .name("age")
+                .build();
+        attrRepository.save(attr);
+        assertThat(attr.getId()).isGreaterThan(0L);
+    }
+
+    @DisplayName("select count(*) from attr")
+    @Test
+    void 테스트카운트(){
+        assertThat(attrRepository.count()).isEqualTo(0);
+    }
 }
