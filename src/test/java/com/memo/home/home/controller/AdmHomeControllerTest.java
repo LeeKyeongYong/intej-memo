e192e7a16e2e0af89ade107703cdd97f43c5075f
이경용 <sleekydz86@icloud.com>
2023-12-09 21:12:25 +0900

    controller 누락추가
    도메인 누락 추가

--- src/test/java/com/memo/home/home/controller/AdmHomeControllerTest.java
+++ src/test/java/com/memo/home/home/controller/AdmHomeControllerTest.java
@@ -1,7 +1,7 @@
 package com.memo.home.home.controller;
 
 
-import com.memo.intejmemo.controller.AdmHomeController;
+import com.memo.intejmemo.domain.home.home.controller.AdmHomeController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
